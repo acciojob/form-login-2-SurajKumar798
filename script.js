4f999f013e11607bd87662e7b6cae43f61c7bfb1
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2025-05-29 12:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const button = document.getElementById('btn');
+const fname = document.getElementById('fname');
+const lname =  document.getElementById('lname');
+const phone =  document.getElementById('phone');
+const email =  document.getElementById('email');
+
+
+
+function submit(){
+	const message = `First Name: ${fname.value} Last Name: ${lname.value} Phone Number: ${phone.value} Email ID: ${email.value}`
+	alert(message);
+}
